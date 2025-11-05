a14e622fe45954d4671d4473a67daa99d37ccbe0
aliuq <aliuq@bilishare.com>
2025-11-05 03:06:27 +0000

    feat: 添加测试工具和重复检查脚本

---
+++ test/common/repeated.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+source ./test-utils.sh
+
+# check the default shell is zsh
+check "default shell is zsh" bash -c 'echo $SHELL' | grep -q 'zsh$'
+# check .zshrc exists
+check "zshrc exists" bash -c 'test -f ~/.zshrc'
+
+# fzf
+check "fzf installed" fzf --version
+# eza
+check "eza installed" eza --version
+# zoxide
+check "zoxide installed" zoxide --version
+# mise
+check "mise installed" mise --version
+# starship
+check "starship installed" starship --version
+
+reportResults
